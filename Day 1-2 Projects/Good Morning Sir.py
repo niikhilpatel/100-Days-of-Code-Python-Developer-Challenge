843ba61508507023f6ed5529354c1842c0ad50a6
Nikhil Patel <90297144+niikhilpatel@users.noreply.github.com>
2022-12-30 15:58:02 +0530

    good morninig time stamp program

---
+++ Day 1-2 Projects/Good Morning Sir.py
@@ -0,0 +1,8 @@
+import time
+timestamp = time.strftime('`%H:%M:%S')
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
